1fb5cfc2c2eec13108d3b5a5df13d0d2f854d03f
sam3125C <samyang3125c@gmail.com>
2021-02-23 11:08:06 +0800

    [Fix] 调整 build.sh，不同的操作系统，不同的 python 版本，build 下的 lib 后缀名不同。

--- build.sh
+++ build.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-# variable define
+# source code path
 work_path=$(cd $(dirname $0); pwd)
-relative_source_src="/src"
-absolute_source_src="${work_path}${relative_source_src}"
-relative_so_src="/build/lib.linux-x86_64-3.6/src"
-absolute_so_src="${work_path}${relative_so_src}"
+source_src_path="${work_path}/src"
 
 echo "[INFO] begin make so files..."
 export PYTHONPATH=$work_path && python3 build.py build_ext
 echo "[INFO] success make so files."
 
+# compiled file path
+compiled_src_path="$(find "${work_path}/build" -type d -name "lib*")/src"
+
 echo "[INFO] begin handle main.pyc..."
-export PYTHONPATH=$work_path && python3 -m compileall -b "${absolute_source_src}/main.py"
-cp "${absolute_source_src}/main.pyc" "${absolute_so_src}/main.pyc"
+export PYTHONPATH=$work_path && python3 -m compileall -b "${source_src_path}/main.py"
+cp "${source_src_path}/main.pyc" "${compiled_src_path}/main.pyc"
 echo "[INFO] succeed handle main.pyc."
 
 echo "[INFO] begin merge deliverd src dir..."
-find ${absolute_source_src} -type d -name "__pycache__" -exec rm -rf {} \;
-find ${absolute_source_src} -type f -name "*.py" -exec rm {} -f \;
-find ${absolute_source_src} -type f -name "*.c" -exec rm {} -f \;
-cp -r ${absolute_so_src}/* ${absolute_source_src}/
+find ${source_src_path} -type d -name "__pycache__" -exec rm -rf {} \;
+find ${source_src_path} -type f -name "*.py" -exec rm {} -f \;
+find ${source_src_path} -type f -name "*.c" -exec rm {} -f \;
+cp -r ${compiled_src_path}/* ${source_src_path}/
 echo "[INFO] succeed merge deliverd src dir."
 
 echo "[INFO] begin delete undeliverd files..."
@@ -30,4 +30,3 @@ do
     rm -rf ${work_path}/$i
 done
 echo "[INFO] success delete undeliverd files."
-
